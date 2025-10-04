3e406ce5324e460ee00676d6e07aeabdf15d7755
abhi-octobit8 <abhishek.srivastava@octobit8.com>
2025-10-04 16:13:44 +0530

    Initial version

---
+++ src/main/java/com/octobit8/multi_speciality_hospital/MultiSpecialityHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.octobit8.multi_speciality_hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiSpecialityHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiSpecialityHospitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/octobit8/multi_speciality_hospital/MultiSpecialityHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octobit8.multi_speciality_hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiSpecialityHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
